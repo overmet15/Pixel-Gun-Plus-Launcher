a02e7e340a7da1e9623384b31f8d9ccb94831c45
overmet15 <metsocool675@gmail.com>
2025-01-30 17:35:18 +0900

    1.3.1 RC

--- Assets/Scripts/Global.cs
+++ Assets/Scripts/Global.cs
@@ -12,8 +12,8 @@ public static class Global
     public const string prewievImageLink = "https://pixelgun.plus/~1031/Screenshots/";
     //public const string newsLink = "https://pixelgun.plus/~1031/pixelgun3d-config/lobbyNews/LobbyNews_all.json";
     public const string versionLink = "https://pixelgun.plus/~1031/Downloads/version.txt";
-    public const string gameDownloadLink = "C:/Users/PC/Downloads/test.zip";
-    //public const string gameDownloadLink = "https://pixelgun.plus/~1031/Downloads/Windows/game.zip";
+    //public const string gameDownloadLink = "C:/Users/PC/Downloads/test.zip";
+    public const string gameDownloadLink = "https://pixelgun.plus/~1031/Downloads/Windows/game.zip";
     
     public static string GameFolderPath => Application.streamingAssetsPath + "/../../Game";
     public static string GameExePath => GameFolderPath + "/Pixel Gun Plus.exe";
