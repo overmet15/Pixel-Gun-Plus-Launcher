3971470ed7ccac12f61d2701ae2c9f27c9caea49
overmet15 <metsocool675@gmail.com>
2025-02-01 16:25:05 +0900

    News parsing W.I.P

--- Assets/Scripts/News/NewsController.cs
+++ Assets/Scripts/News/NewsController.cs
@@ -1,16 +1,74 @@
 using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using SimpleJSON;
+using System;
 
 public class NewsController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private UIGrid spawnItemsIn;
+    [SerializeField] private GameObject ogItem;
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Start()
     {
-        
+        //yield break;
+
+        UnityWebRequest request = UnityWebRequest.Get(Global.newsLink);
+
+        yield return request.SendWebRequest();
+
+        if (request.result != UnityWebRequest.Result.Success)
+        {
+            Debug.LogWarning("Couldnt get news");
+            yield break;
+        }
+
+        try
+        {
+            JSONNode index = JSON.Parse(request.downloadHandler.text);
+
+            foreach (JSONNode node in index)
+            {
+                NewsData data = new();
+
+                long unixDate = node["date"];
+                data.date = DateTimeOffset.FromUnixTimeSeconds(unixDate).UtcDateTime;
+                
+                data.url = node["URL"];
+
+                data.previewPicture = node["previewpicture"];
+                data.fullpicture = node["fullpicture"];
+
+                data.shortHeader = node["short_header"]["English"];
+                data.header = node["header"]["English"];
+
+                data.shortDescription = node["short_description"]["English"];
+                data.description = node["description"]["English"];
+
+                data.category = node["category"]["English"];
+
+                //NewsItem obj = Instantiate(ogItem, spawnItemsIn.transform).GetComponent<NewsItem>();
+                NewsItem obj =  NGUITools.AddChild(spawnItemsIn.gameObject, ogItem).GetComponent<NewsItem>();
+                obj.data = data;
+                obj.UpdateDisplay();
+                spawnItemsIn.Reposition();
+            }
+        }
+        catch
+        {
+            Debug.LogWarning("Exception on news parsing");
+            yield break;
+        }
     }
+}
+
+#if UNITY_EDITOR
+[System.Serializable] // Serialize in editor
+#endif
+public struct NewsData
+{
+    public DateTime date;
+    public string url;
+    public string previewPicture, fullpicture;
+    public string shortHeader, header, shortDescription, description, category;
 }
--- Assets/Scripts/News/NewsItem.cs
+++ Assets/Scripts/News/NewsItem.cs
@@ -2,5 +2,18 @@ using UnityEngine;
 
 public class NewsItem : MonoBehaviour
 {
-    public UILabel header, shortDesc, date;
+    public NewsData data;
+    [SerializeField] private UILabel header, shortDesc, date;
+
+    public void UpdateDisplay()
+    {
+        header.text = data.shortHeader;
+        shortDesc.text = data.shortDescription;
+        date.text = $"{data.date.Day}.{data.date.Month}.{data.date.Year}";
+    }
+
+    public void OnPressed()
+    {
+        // Logic here
+    }
 }
